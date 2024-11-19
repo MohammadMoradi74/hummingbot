516bd9336e21127bf08e15b85546e37e2ec0ea66
mohammad <mamadquant@gmail.com>
2024-11-19 11:52:44 +0330

    Update BitpinAuth to send request for authentication

--- hummingbot/connector/exchange/bitpin/bitpin_auth.py
+++ hummingbot/connector/exchange/bitpin/bitpin_auth.py
@@ -1,13 +1,10 @@
-import hashlib
-import hmac
-import json
-from collections import OrderedDict
-from typing import Any, Dict
-from urllib.parse import urlencode
+from typing import Dict
+
+import aiohttp
 
 from hummingbot.connector.time_synchronizer import TimeSynchronizer
 from hummingbot.core.web_assistant.auth import AuthBase
-from hummingbot.core.web_assistant.connections.data_types import RESTMethod, RESTRequest, WSRequest
+from hummingbot.core.web_assistant.connections.data_types import RESTRequest, WSRequest
 
 
 class BitpinAuth(AuthBase):
@@ -15,6 +12,8 @@ class BitpinAuth(AuthBase):
         self.api_key = api_key
         self.secret_key = secret_key
         self.time_provider = time_provider
+        self.access_token = None
+        self.refresh_token = None
 
     async def rest_authenticate(self, request: RESTRequest) -> RESTRequest:
         """
@@ -22,10 +21,8 @@ class BitpinAuth(AuthBase):
         the required parameter in the request header.
         :param request: the request to be configured for authenticated interaction
         """
-        if request.method == RESTMethod.POST:
-            request.data = self.add_auth_to_params(params=json.loads(request.data))
-        else:
-            request.params = self.add_auth_to_params(params=request.params)
+        if self.access_token is None:
+            await self.authenticate()
 
         headers = {}
         if request.headers is not None:
@@ -42,23 +39,30 @@ class BitpinAuth(AuthBase):
         """
         return request  # pass-through
 
-    def add_auth_to_params(self,
-                           params: Dict[str, Any]):
-        timestamp = int(self.time_provider.time() * 1e3)
-
-        request_params = OrderedDict(params or {})
-        request_params["timestamp"] = timestamp
-
-        signature = self._generate_signature(params=request_params)
-        request_params["signature"] = signature
-
-        return request_params
-
     def header_for_authentication(self) -> Dict[str, str]:
-        return {"Content-Type": "application/json"}
+        return {"Content-Type": "application/json",
+                "Authorization": f"Bearer {self.access_token}"}
 
-    def _generate_signature(self, params: Dict[str, Any]) -> str:
+    async def authenticate(self):
+        """
+        Sends the authentication request to the Bitpin API to get the access and refresh tokens.
+        """
+        # TODO: Clean up the mess!
+        url = "https://api.bitpin.ir/api/v1/usr/authenticate/"
+        headers = {
+            "Content-Type": "application/json"
+        }
+        payload = {
+            "api_key": self.api_key,
+            "secret_key": self.secret_key
+        }
 
-        encoded_params_str = urlencode(params)
-        digest = hmac.new(self.secret_key.encode("utf8"), encoded_params_str.encode("utf8"), hashlib.sha256).hexdigest()
-        return digest
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url, json=payload, headers=headers) as response:
+                if response.status == 200:
+                    data = await response.json()
+                    self.access_token = data.get("access")
+                    self.refresh_token = data.get("refresh")
+                    print("Authentication successful!")
+                else:
+                    raise Exception(f"Authentication failed with status code {response.status}")
