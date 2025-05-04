2cb959acc34d20feb48346da2568c33a2838a456
mohammad <mamadquant@gmail.com>
2025-05-04 15:56:19 +0330

    Remove extra domain config map

--- hummingbot/connector/exchange/bitpin/bitpin_utils.py
+++ hummingbot/connector/exchange/bitpin/bitpin_utils.py
@@ -54,39 +54,3 @@ class BitpinConfigMap(BaseConnectorConfigMap):
 
     class Config:
         title = "bitpin"
-
-
-KEYS = BitpinConfigMap.construct()
-
-OTHER_DOMAINS = ["bitpin_org"]
-OTHER_DOMAINS_PARAMETER = {"bitpin_org": "org"}
-OTHER_DOMAINS_EXAMPLE_PAIR = {"bitpin_org": "BTC-USDT"}
-OTHER_DOMAINS_DEFAULT_FEES = {"bitpin_org": DEFAULT_FEES}
-
-
-class BitpinUSConfigMap(BaseConnectorConfigMap):
-    connector: str = Field(default="bitpin_org", const=True, client_data=None)
-    bitpin_api_key: SecretStr = Field(
-        default=...,
-        client_data=ClientFieldData(
-            prompt=lambda cm: "Enter your Bitpin US API key",
-            is_secure=True,
-            is_connect_key=True,
-            prompt_on_new=True,
-        )
-    )
-    bitpin_api_secret: SecretStr = Field(
-        default=...,
-        client_data=ClientFieldData(
-            prompt=lambda cm: "Enter your Bitpin US API secret",
-            is_secure=True,
-            is_connect_key=True,
-            prompt_on_new=True,
-        )
-    )
-
-    class Config:
-        title = "bitpin_org"
-
-
-OTHER_DOMAINS_KEYS = {"bitpin_org": BitpinUSConfigMap.construct()}
