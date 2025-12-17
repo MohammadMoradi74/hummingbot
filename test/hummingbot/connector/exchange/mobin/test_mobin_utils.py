cda484415ed832244ab2e428426e1b454489e9e1
mohammad <mamadquant@gmail.com>
2025-12-17 16:10:54 +0330

    Add EXAMPLE_PAIR and fee

--- test/hummingbot/connector/exchange/mobin/test_mobin_utils.py
+++ test/hummingbot/connector/exchange/mobin/test_mobin_utils.py
@@ -1,18 +1,22 @@
 import unittest
 
-from hummingbot.connector.exchange.binance import binance_utils as utils
+from hummingbot.connector.exchange.mobin import mobin_utils as utils
 
 
-class BinanceUtilTestCases(unittest.TestCase):
+class MobinUtilTestCases(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
         super().setUpClass()
-        cls.base_asset = "COINALPHA"
-        cls.quote_asset = "HBOT"
+        cls.base_asset = "IRTKMOFD0001"
+        cls.quote_asset = "IRR"
         cls.trading_pair = f"{cls.base_asset}-{cls.quote_asset}"
         cls.hb_trading_pair = f"{cls.base_asset}-{cls.quote_asset}"
+        # TODO: Find the ex_trading_pair, guess it is IRTKMOFD0001 as there is only one quote asset
         cls.ex_trading_pair = f"{cls.base_asset}{cls.quote_asset}"
+
+    # TODO: For now ignoring the test. This is the api:
+    #  https://qcore.mobinsb.ir/v1/Instruments/Information?id=IRTKMOFD0001
 
     def test_is_exchange_information_valid(self):
         invalid_info_1 = {
