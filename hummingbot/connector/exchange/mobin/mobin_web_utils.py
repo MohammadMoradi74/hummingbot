aecc9444a023437e25968c859ebbdc8208679506
MohammadMoradi <mohammadmoradi.ee@gmail.com>
2026-02-25 13:37:53 +0330

    Update get_current_server_time

--- hummingbot/connector/exchange/mobin/mobin_web_utils.py
+++ hummingbot/connector/exchange/mobin/mobin_web_utils.py
@@ -1,3 +1,4 @@
+import time
 from typing import Callable, Optional
 
 import hummingbot.connector.exchange.mobin.mobin_constants as CONSTANTS
@@ -5,7 +6,6 @@ from hummingbot.connector.time_synchronizer import TimeSynchronizer
 from hummingbot.connector.utils import TimeSynchronizerRESTPreProcessor
 from hummingbot.core.api_throttler.async_throttler import AsyncThrottler
 from hummingbot.core.web_assistant.auth import AuthBase
-from hummingbot.core.web_assistant.connections.data_types import RESTMethod
 from hummingbot.core.web_assistant.web_assistants_factory import WebAssistantsFactory
 
 
@@ -64,13 +64,15 @@ async def get_current_server_time(
         throttler: Optional[AsyncThrottler] = None,
         domain: str = CONSTANTS.DEFAULT_DOMAIN,
 ) -> float:
-    throttler = throttler or create_throttler()
-    api_factory = build_api_factory_without_time_synchronizer_pre_processor(throttler=throttler)
-    rest_assistant = await api_factory.get_rest_assistant()
-    response = await rest_assistant.execute_request(
-        url=public_rest_url(path_url=CONSTANTS.SERVER_TIME_PATH_URL, domain=domain),
-        method=RESTMethod.GET,
-        throttler_limit_id=CONSTANTS.SERVER_TIME_PATH_URL,
-    )
-    server_time = response["serverTime"]
+    # throttler = throttler or create_throttler()
+    # api_factory = build_api_factory_without_time_synchronizer_pre_processor(throttler=throttler)
+    # rest_assistant = await api_factory.get_rest_assistant()
+    # response = await rest_assistant.execute_request(
+    #     url=public_rest_url(path_url=CONSTANTS.SERVER_TIME_PATH_URL, domain=domain),
+    #     method=RESTMethod.GET,
+    #     throttler_limit_id=CONSTANTS.SERVER_TIME_PATH_URL,
+    #     is_auth_required=True,
+    # )
+    # server_time = response["serverTime"]
+    server_time = time.time() * 1e3
     return server_time
