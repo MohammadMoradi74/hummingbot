05c2a5628ba8ee064065aaa64c31ce9715b58eae
MohammadMoradi <mohammadmoradi.ee@gmail.com>
2026-06-07 19:10:27 +0330

    Remove extra comments

--- hummingbot/connector/exchange/mobin/mobin_api_order_book_data_source.py
+++ hummingbot/connector/exchange/mobin/mobin_api_order_book_data_source.py
@@ -175,9 +175,7 @@ class MobinAPIOrderBookDataSource(OrderBookTrackerDataSource):
             message_queue.put_nowait(order_book_message)
 
     def _channel_originating_message(self, event_message: Dict[str, Any]) -> str:
-        print(event_message)
         message = json.loads(event_message.rstrip('\x1e\x00\x1f'))
-        print("decoded message: {}".format(message))
         channel = ""
         if 'time' in message:
             return channel
