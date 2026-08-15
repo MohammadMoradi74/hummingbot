a9ab999b09618880fcb347cd191573f3e16b5966
MohammadMoradi <mohammadmoradi.ee@gmail.com>
2026-08-15 18:14:12 +0330

    Update test_nobitex_utils.py

--- test/hummingbot/connector/exchange/nobitex/test_nobitex_utils.py
+++ test/hummingbot/connector/exchange/nobitex/test_nobitex_utils.py
@@ -8,37 +8,17 @@ class NobitexUtilTestCases(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         super().setUpClass()
-        cls.base_asset = "COINALPHA"
-        cls.quote_asset = "HBOT"
+        cls.base_asset = "BTC"
+        cls.quote_asset = "IRT"
         cls.trading_pair = f"{cls.base_asset}-{cls.quote_asset}"
-        cls.hb_trading_pair = f"{cls.base_asset}-{cls.quote_asset}"
-        cls.ex_trading_pair = f"{cls.base_asset}{cls.quote_asset}"
 
     def test_is_exchange_information_valid(self):
-        invalid_info_1 = {
-            "status": "BREAK",
-            "permissionSets": [["MARGIN"]],
-        }
+        self.assertTrue(utils.is_exchange_information_valid("BTCIRT"))
+        self.assertFalse(utils.is_exchange_information_valid(""))
+        self.assertFalse(utils.is_exchange_information_valid(None))
 
-        self.assertFalse(utils.is_exchange_information_valid(invalid_info_1))
-
-        invalid_info_2 = {
-            "status": "BREAK",
-            "permissionSets": [["SPOT"]],
-        }
-
-        self.assertFalse(utils.is_exchange_information_valid(invalid_info_2))
-
-        invalid_info_3 = {
-            "status": "TRADING",
-            "permissionSets": [["MARGIN"]],
-        }
-
-        self.assertFalse(utils.is_exchange_information_valid(invalid_info_3))
-
-        invalid_info_4 = {
-            "status": "TRADING",
-            "permissionSets": [["SPOT"]],
-        }
-
-        self.assertTrue(utils.is_exchange_information_valid(invalid_info_4))
+    def test_exchange_symbol_helpers(self):
+        self.assertEqual("BTCIRT", utils.exchange_symbol_for_tokens("BTC", "IRT"))
+        self.assertEqual(("BTC", "IRT"), utils.split_exchange_symbol("BTCIRT"))
+        self.assertEqual(("btc", "rls"), utils.currencies_from_trading_pair("BTC-IRT"))
+        self.assertEqual("btc-rls", utils.stats_market_key("BTC-IRT"))
