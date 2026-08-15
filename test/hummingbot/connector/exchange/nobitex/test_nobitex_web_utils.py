941499c96e0af6d4fd0e24922496879a111bd12a
MohammadMoradi <mohammadmoradi.ee@gmail.com>
2026-08-15 18:13:57 +0330

    Update test_nobitex_web_utils.py

--- test/hummingbot/connector/exchange/nobitex/test_nobitex_web_utils.py
+++ test/hummingbot/connector/exchange/nobitex/test_nobitex_web_utils.py
@@ -13,9 +13,7 @@ class NobitexUtilTestCases(unittest.IsolatedAsyncioTestCase):
     def test_public_rest_url(self):
         path_url = "/TEST_PATH"
         domain = "ir"
-        expected_url = CONSTANTS.REST_URL.format(domain) + CONSTANTS.PUBLIC_API_VERSION + path_url
-        self.assertEqual(expected_url, web_utils.public_rest_url(path_url, domain))
-        self.assertEqual("https://apiv2.nobitex.ir/v3/TEST_PATH", web_utils.public_rest_url(path_url, domain))
+        self.assertEqual("https://apiv2.nobitex.ir/TEST_PATH", web_utils.public_rest_url(path_url, domain))
 
     def test_private_rest_url(self):
         path_url = "/TEST_PATH"
